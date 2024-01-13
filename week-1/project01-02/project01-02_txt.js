49938a6684f40c640f354144f4ea4ab6b76728de
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-01-13 12:46:47 -1000

    week 1 changes

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Leah Harris
+    Date:  01-10-24
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
